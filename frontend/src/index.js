1942fe13570197f5bf5ab50459c0091c247a6cdc
Natalia <a28710@svalero.com>
2025-01-08 12:56:23 +0100

    nuevos cambios de frontend que han roto lo de grabar pelis y que no funciona delete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,50 @@
 import axios from 'axios';
+import { el, icon } from './documentUtil';
+import  { notifyOk } from './documentUtil';
 
-window.readMovies = function() {
+window.readMovies = function () {
     axios.get('http://localhost:8080/movies')
         .then((response) => {
             const movieList = response.data;
-            const movieUl = document.getElementById('movies');
+            //const movieUl = el('movies'); Así era cuando lo teníamos en forma de lista en index.html
+            const movieTable = el('tableBody');
             movieList.forEach(movie => {
-                const listItem = document.createElement('li');
-                listItem.className = 'list-group-item';
-                listItem.appendChild(document.createTextNode(movie.title + ' (' + movie.year + ') ' + movie.description));
-                movieUl.appendChild(listItem);
+                //const listItem = document.createElement('li');  Pasa lo mismo que con lo de arriba, esto era para cuando teníamos la lista
+                //listItem.className = 'list-group-item';
+                //listItem.appendChild(document.createTextNode(movie.title + ' (' + movie.year + ') ' + movie.description));
+                //movieUl.appendChild(listItem);
 
-            })
+                const row = document.createElement('tr');
+                row.id = 'movie-' + movie.id;
+                row.innerHTML = '<td>' + movie.title + '</td>' + //Webinar 4, en minuto 52 se lleva los td también en una función de documentUtil.js
+                                '<td>' + movie.description + '</td>' +
+                                '<td>' + movie.year + '</td>' +
+                                '<a class="btn btn-warning" href="#">' + 
+                                icon('edit') //Antes estaba aquí el código svg pero nos lo hemos llevado a documentUtil.js
+                               + '</a>' +
+                                '<a class="btn btn-danger" href="javascript:removeMovie(' + movie.id + ');">' + 
+                                icon('delete') //Antes estaba aquí código svg, ahora en documentUtil.js
+                               + '</a>';
                 
+                movieTable.appendChild(row);
+            })
+
         });
-        
-}
+};
+
+window.removeMovie = function(id) {
+    if(confirm('¿Está seguro de que desea eliminar esta película?')) {
+    axios.delete('http://localhost:8080/movies/' + id)
+        .then((response) => {
+            if (response.status == 204){
+                notifyOk('Película eliminada correctamente');
+                el('movie-' + id).remove();
+            }
+            else {
+                notifyOk('Nada');
+            }
+        }).catch(error => {
+            console.error(error);
+          });
+    }
+};
--- frontend/src/registro.js
+++ frontend/src/registro.js
@@ -1,10 +1,11 @@
 import axios from 'axios'; //Axios es la librería que me permite comunicarme con el backend
 import { notifyError, notifyOk } from './util.js';
+import { el } from './documentUtil.js';
 
 window.addMovie = function () { //Programo lo que va a suceder cuando se clica en el botón del formulario para hacer un nuevo registro
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
-    const year = document.getElementById('year').value;
+    const title = el('title').value;
+    const description = el('description').value;
+    const year = el('year').value;
 
 
     //TO DO Validación de los datos que introduce el usuario
@@ -21,11 +22,11 @@ window.addMovie = function () { //Programo lo que va a suceder cuando se clica e
     });
 
     //Decir al usuario si la entrada ha tenido lugar
-   notifyOk('Película registrada');
+    notifyOk('Película registrada');
 
     //Limpiar el formulario después de registrar nueva entrada:
-    document.getElementById('title').value = '';
-    document.getElementById('description').value = '';
-    document.getElementById('year').value = '';
+    el('title').value = '';
+    el('description').value = '';
+    el('year').value = '';
 
 };
